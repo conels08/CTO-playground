a8f6553443c5613df28753f9dcc954a7c9cce6dd
Colby Nelsen <colby.nelsen@gmail.com>
2026-01-10 21:17:46 -0800

    fix: remove invalid border attrs from PayPal form

--- components/SupportThisProject.tsx
+++ components/SupportThisProject.tsx
@@ -16,8 +16,8 @@ export default function SupportThisProject() {
           <input type="hidden" name="no_recurring" value="0" />
           <input type="hidden" name="item_name" value="To provide free, useful online tools for as many people as possible." />
           <input type="hidden" name="currency_code" value="USD" />
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+          <img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </form>
 
       </div>
